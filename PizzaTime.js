5acbc8b9671c1eb28adf8c2c4352b1138e01c204
Joseph Turbin <joseph.turbin@gmail.com>
2014-08-25 19:57:06 -0700

    updates joe made

--- PizzaTime.js
+++ PizzaTime.js
@@ -1,8 +1,23 @@
 $(function(){
-	$(".pizzaSelection").hide();
-
+	
 	$("#cities").change(function (){
 		$(".pizzaSelection").hide();
 		$("#"+this.value).show();
+
 	});
+
+	$(".pizzaSelection").change(function (){
+		$(".ingredients").hide();
+		$(".ingredientContainer").show();
+		$("#"+this.value).show();
+	});
+
+	$("button").click(function(){
+		$(".ingredients").hide();
+		$(".ingredientContainer").hide();
+		$(".pizzaSelection").hide();
+
+	});
+
+
 });
